8da0f9fe0290b54ec4a3625f98f3673fde57d0c9
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2024-04-08 19:26:30 -0300

    Fixed

--- src/services/UsuarioService.ts
+++
@@ -1,9 +0,0 @@
-import { Usuario } from "../business/entities/Usuario";
-import { UsuarioRequestDto } from "../dto/UsuarioRequestDto";
-
-export interface UsuarioService {
-    criaUsuario(usuarioRequestDto: UsuarioRequestDto) : Promise<Usuario>;
-    listaUsuarios() : Promise<Usuario[]>;
-    pegaUsuario(id: string) : Promise<Usuario | undefined>;
-    deletaUsuario(id: string) : Promise<void>
-}
